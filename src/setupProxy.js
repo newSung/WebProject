b9f5bc5542b5309de341fe5cb1199f98785d6507
newSung <108291773+newSung@users.noreply.github.com>
2023-08-21 13:01:24 +0900

    Update setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -12,5 +12,5 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
-    app.use(createProxyMiddleware('/api', { target: "https://port-0-servertest-619z9h2lllkaic66.sel3.cloudtype.app/", changeOrigin: true }));
+    app.use(createProxyMiddleware('/api', { target: "http://localhost:5000", changeOrigin: true }));
 };
