3423136d0533a93ae11db783c96ed79a816ea712
newSung <gho0612@gmail.com>
2023-08-21 22:48:09 +0900

    x

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -9,10 +9,10 @@
 //     );
 // };
 
-const { createProxyMiddleware } = require('http-proxy-middleware');
+// const { createProxyMiddleware } = require('http-proxy-middleware');
 
-module.exports = function (app) {
-    // app.use(createProxyMiddleware('/api', { target: "http://localhost:5000", changeOrigin: true }));
-    app.use(createProxyMiddleware('/api', { target: "https://port-0-servertest-619z9h2lllkaic66.sel3.cloudtype.app", changeOrigin: true }));
-};
+// module.exports = function (app) {
+//     // app.use(createProxyMiddleware('/api', { target: "http://localhost:5000", changeOrigin: true }));
+//     app.use(createProxyMiddleware('/api', { target: "https://port-0-servertest-619z9h2lllkaic66.sel3.cloudtype.app", changeOrigin: true }));
+// };
 
