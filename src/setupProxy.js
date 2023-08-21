9eda76216f0bdc54787e09e7b56ef64c021ec0a4
newSung <gho0612@gmail.com>
2023-08-21 16:43:12 +0900

    a

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -12,7 +12,6 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
-
     // app.use(createProxyMiddleware('/api', { target: "http://localhost:5000", changeOrigin: true }));
     app.use(createProxyMiddleware('/api', { target: "https://port-0-servertest-619z9h2lllkaic66.sel3.cloudtype.app", changeOrigin: true }));
 };
