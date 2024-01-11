cd4ea96b7d0c031dd4056db5051bc0eb440140d5
newSung <gho0612@gmail.com>
2024-01-11 22:11:14 +0900

    test

--- src/components/axios-config.js
+++ src/components/axios-config.js
@@ -7,7 +7,8 @@ import axios from 'axios';
 
 //배포
 const axiosInstance = axios.create({
-    baseURL: 'https://port-0-servertest-619z9h2lllkaic66.sel3.cloudtype.app'
+    baseURL: 'https://port-0-servertest-619z9h2lllkaic66.sel3.cloudtype.app',
+    withCredentials: true,
 });
 
 export default axiosInstance;   
